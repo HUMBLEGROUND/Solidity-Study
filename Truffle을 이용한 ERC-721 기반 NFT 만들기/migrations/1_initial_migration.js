ab5c68d0d7bbbf9cde3a1b40ea519194a45833dc
wavescats <wavescats@gmail.com>
2022-06-10 15:25:51 +0900

    truffle-erc721-NFT-deploy

--- Truffle을 이용한 ERC-721 기반 NFT 만들기/migrations/1_initial_migration.js
+++ Truffle을 이용한 ERC-721 기반 NFT 만들기/migrations/1_initial_migration.js
@@ -3,5 +3,5 @@ const MyNFTs = artifacts.require("MyNFTs.sol"); // MyNFTs.sol 파일 추가
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(MyNFTs); // MyNFTs를 배포에 추가
+  deployer.deploy(MyNFTs); // MyNFTs를 배포에 추가 
 };
